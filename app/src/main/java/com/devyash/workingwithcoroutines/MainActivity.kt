5fd571bc09c9c5c35ad0640f4cb5723a5c7ba5be
Yashveer singh <yashveersingh30401@gmail.com>
2023-05-22 18:48:31 +0530

    docs - try threading

--- app/src/main/java/com/devyash/workingwithcoroutines/MainActivity.kt
+++ app/src/main/java/com/devyash/workingwithcoroutines/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.devyash.workingwithcoroutines.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding:ActivityMainBinding?=null
+    private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +20,25 @@ class MainActivity : AppCompatActivity() {
             updateCounter()
         }
 
+        binding.btnLongTask.setOnClickListener {
+            startLongRunningTask()
+        }
     }
 
     private fun updateCounter() {
-        Log.d("THREADNAME",Thread.currentThread().name)
+        Log.d("THREADNAME", Thread.currentThread().name)
         var count = binding.tvCounter.text.toString().toInt()
         count++
         binding.tvCounter.text = count.toString()
+    }
+
+    private fun startLongRunningTask() {
+        thread(start = true) {
+            Log.d("THREADNAME",Thread.currentThread().name)
+            for (i in 1..1000000000L) {
+
+            }
+        }
     }
 
     override fun onDestroy() {
